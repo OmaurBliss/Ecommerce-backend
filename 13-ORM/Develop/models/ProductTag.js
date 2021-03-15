8502fbf47e6c9000d5541f19c4ef170a3f032d2d
Omaur B <omaurbliss@gmail.com>
2021-03-15 16:06:02 -0500

    added product tag model

--- 13-ORM/Develop/models/ProductTag.js
+++ 13-ORM/Develop/models/ProductTag.js
@@ -9,6 +9,14 @@ ProductTag.init(
     // define columns
   },
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+  },
+  {
     sequelize,
     timestamps: false,
     freezeTableName: true,
