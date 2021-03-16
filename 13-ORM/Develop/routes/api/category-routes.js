e71ca17ff47f63b7c92c25d76cd0fd0617e16b1a
Omaur B <omaurbliss@gmail.com>
2021-03-16 08:41:32 -0500

    added create category function

--- 13-ORM/Develop/routes/api/category-routes.js
+++ 13-ORM/Develop/routes/api/category-routes.js
@@ -24,7 +24,7 @@ router.get('/:id', async (req, res) => {
     });
 
     if (!categoryrData) {
-      res.status(404).json({ message: 'No traveller found with this id!' });
+      res.status(404).json({ message: 'No category found with this id!' });
       return;
     }
 
@@ -33,9 +33,14 @@ router.get('/:id', async (req, res) => {
     res.status(500).json(err);
   }
 });
-
-router.post('/', (req, res) => {
-  // create a new category
+ // create a new category
+ router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
