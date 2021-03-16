008a88d7f9b9071c275514bdc69a5cdc8b554ddc
Omaur B <omaurbliss@gmail.com>
2021-03-16 08:48:51 -0500

    added update and delete category functions

--- 13-ORM/Develop/routes/api/category-routes.js
+++ 13-ORM/Develop/routes/api/category-routes.js
@@ -42,13 +42,34 @@ router.get('/:id', async (req, res) => {
     res.status(400).json(err);
   }
 });
+// update a category by its `id` value
+router.put('/', async (req, res) => {
+  try {
+    const categoryData = await Category.update(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+ // delete a category by its `id` value
+ router.delete('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
-});
 
 module.exports = router;
